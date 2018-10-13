2cb26651fdf632ffa5a697546d2f12282e142dba
sundeep <sunguru98@yahoo.co.in>
2018-10-13 23:25:00 +0530

    Added sockets and established socket connection on both client as well as server side

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const path = require("path");
-
+const http = require("http");
+const socketIO = require("socket.io");
 let port = process.env.PORT||3000;
 const publicPath = path.join(__dirname,"../public");
 console.log(publicPath);
 
-
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+io.on("connection",(socket)=>{
+    console.log("A new user is connected !");
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected");
+    })
+})
+
+server.listen(port,()=>{
     console.log(`Server connected to ${port}`);
 })
