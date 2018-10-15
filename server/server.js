0a4440f5b2ae17d1d9ff5ad33be92abe85a6e116
sundeep <sunguru98@yahoo.co.in>
2018-10-15 21:14:33 +0530

    Greet new user and notify other users that a new user has joined

--- server/server.js
+++ server/server.js
@@ -10,11 +10,21 @@ let server = http.createServer(app);
 let io = socketIO(server);
 app.use(express.static(publicPath));
 io.on("connection",(socket)=>{
-    console.log(socket);
     console.log("A new user is connected !");
     socket.on("disconnect",()=>{
         console.log("User Disconnected");
     }); 
+
+    socket.emit("newMessage",{
+        from:"admin",
+        message:"Welcome To the chat room",
+        createdAt:new Date().getTime(),
+    });
+
+    socket.broadcast.emit("newMessage",{
+        from:"admin",
+        text:"A new user joined",
+    });
 
     socket.on("createMessage",(message)=>{
         console.log("createMessage",message);
